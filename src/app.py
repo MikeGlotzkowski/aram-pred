ebd76f8d02c76d86fb91169f980efb8649c5a8da
MikeGlotzkowski <sebastian.lischewski@outlook.com>
2020-08-28 21:14:21 +0200

    fix(seed player): skip seed player (is always in the first set of games...)

--- src/app.py
+++ src/app.py
@@ -11,9 +11,13 @@ persistency = Mongo.Mongo(logger)
 seed_encrypted_account_id = '-w9INIopYVNjHShnEGGgdYhREGEW407RXfAG6ltIjfEi_g'
 PlayerCrawler.PlayerCrawler(seed_encrypted_account_id).run()
 
+
 while True:
     match = persistency.get_uncrawled_game(queue_id)
     for participant_identity in match['participantIdentities']:
-        PlayerCrawler.PlayerCrawler(
-            participant_identity['player']['currentAccountId']).run()
+        encrypted_account_id = participant_identity['player']['currentAccountId']
+        if encrypted_account_id == seed_encrypted_account_id:
+            pass
+        else:
+            PlayerCrawler.PlayerCrawler(encrypted_account_id).run()
     persistency.mark_match_as_crawled(match['gameId'])
